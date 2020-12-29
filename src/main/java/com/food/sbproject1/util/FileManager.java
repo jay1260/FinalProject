84eabe4f00d4616b1456df4333b44e7a73253a41
jay1260 <jay1260@naver.com>
2020-12-29 12:51:22 +0900

    done

--- src/main/java/com/food/sbproject1/util/FileManager.java
+++ src/main/java/com/food/sbproject1/util/FileManager.java
@@ -23,18 +23,4 @@ public class FileManager {
 			return fileName;
 	}
 
-	
-	public String saveFileCopy(MultipartFile multipartFile, File file) throws Exception{
-		
-		String fileName = null;
-		
-		// 중복되지 않는 파일명 생성
-		fileName = UUID.randomUUID().toString()+"_"+multipartFile.getOriginalFilename();
-		
-		file = new File(file,fileName);
-		
-		FileCopyUtils.copy(multipartFile.getBytes(), file);
-		
-		return fileName;
-	}
 }
